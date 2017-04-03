76239250c96c0a27247b34ef31c7607e101e6160
Addison C. Quijano Sorca <addison.sorca@gmail.com>
2017-04-03 09:54:33 -0700

    Change styling to original website

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,14 @@ function openMenu() {
   document.getElementsByClassName("menu").style.display = "block";
   document.getElementsByClassName("")
 }
+
+var searchStyle = document.getElementById('search_style');
+document.getElementById('search').addEventListener('input', function() {
+  if (!this.value) {
+    searchStyle.innerHTML = "";
+    return;
+  }
+  // look ma, no indexOf!
+  searchStyle.innerHTML = ".searchable:not([data-index*=\"" + this.value.toLowerCase() + "\"]) { display: none; }";
+  // beware of css injections!
+});
